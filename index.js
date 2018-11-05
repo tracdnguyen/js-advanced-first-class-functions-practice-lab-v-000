05f108000fffa7489f9e7c2f9cc48978e8729839
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-05 04:14:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file!
-function logDriverNames(driver) {
-  
-}
+const logDriverNames = function(drivers) {
+  drivers.forEach(function(driver){
+    console.log(driver.name);
+  });
+};
